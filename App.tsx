558fd0ea129b751ca4e771bf53e62fbf51c176f2
malit mohamut <malitmohamud@gmail.com>
2025-10-11 12:58:35 +0800

    itemns

--- App.tsx
+++ App.tsx
@@ -1181,7 +1181,7 @@ export default function App() {
               </motion.h2>
             </div>
             
-            <div className="grid gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
               {[
                 {
                   icon: Globe,
